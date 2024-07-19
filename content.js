72abc2687b38fea74e369695b9820d9935311524
maximTestGit <32741254+maximTestGit@users.noreply.github.com>
2024-07-19 13:29:57 +0300

    request works!

--- content.js
+++ content.js
@@ -41,9 +41,132 @@ function generateGuid() {
     "TotalDuration": 540.0,
     "Count": 2
   },
+-----------
+{
+  "entries": [
+    {
+      "id": null,
+      "trackingId": 1,
+      "employeeId": 300,
+      "date": "2024-07-01",
+      "start": "10:00",
+      "end": "11:30",
+      "note": "",
+      "projectId": 32,
+      "taskId": 105
+    }
+  ]
+}
+----------------
+Project: 32, "SM9.2.1"
+Task: 105, "BLD - Design, Code, QA, Documentation"
+Task: 107, "OHD - Daily, Planning, Sprint review, Retrospective"
+
 
 */
 async function processDayDataRequest(requestDayData) {
+    const scripts = document.querySelectorAll('script');
+    const scriptWithSessionUser = Array.from(scripts)
+        .find(script => script.textContent.includes('var SESSION_USER'));
+    const sessionUserString = scriptWithSessionUser.textContent.match(/var\s+SESSION_USER\s*=\s*({.+?});/s)[1];
+    const SESSION_USER = JSON.parse(sessionUserString);
+    //printLogContent('info', `SESSION_USER: ${SESSION_USER}`);
+    const employeeId = SESSION_USER.employeeId;
+    printLogContent('info', `employeeId: ${employeeId}`);
+
+    var i = 0;
+    const item = requestDayData.Items[i];
+
+    const dateStartFull = new Date(item.Date);
+    const year = dateStartFull.getFullYear();
+    const month = dateStartFull.getMonth() + 1; // Months are zero-indexed (January = 0)
+    const day = dateStartFull.getDate();
+    
+    const date = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+    printLogContent('info', `date: ${date}`);
+    
+    var hours = dateStartFull.getHours();
+    var minutes = dateStartFull.getMinutes();
+    
+    const start = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+    printLogContent('info', `start: ${start}`);
+
+    var duration = item.TotalDuration;
+    printLogContent('info', `duration: ${duration}`);
+
+    var endTime = new Date(dateStartFull.getTime() + duration * 60*1000);
+    hours = endTime.getHours();
+    minutes = endTime.getMinutes();
+    
+    const end = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+    printLogContent('info', `end: ${end}`);
+
+    const project = 32;
+    const task = 105;
+
+    const requestData = {
+        "entries": [
+            {
+                "id": null,
+                "trackingId": i,
+                "employeeId": employeeId,
+                "date": date,
+                "start": start,
+                "end": end,
+                "note": "",
+                "projectId": project,
+                "taskId": task
+            }
+        ]
+    };
+
+    const body = JSON.stringify(requestData);
+    printLogContent('info', `requestData: ${JSON.stringify(requestData)}`);
+
+    const scriptWithCSRFToken = Array.from(scripts)
+        .find(script => script.textContent.includes('var CSRF_TOKEN'));
+    const CSRF_TOKEN = scriptWithCSRFToken.textContent.match(/var CSRF_TOKEN = "([^"]+)"/)[1];
+    printLogContent('info', `CSRF_TOKEN: ${CSRF_TOKEN}`);
+
+    await submitTimesheetEntry(CSRF_TOKEN, employeeId, body);
+}
+
+async function submitTimesheetEntry(csrf_token, employeeId, body) {
+    try {
+      const response = await fetch("https://qognify.bamboohr.com/timesheet/clock/entries", {
+        method: "POST",
+        headers: {
+          "accept": "application/json, text/plain, */*",
+          "accept-language": "en-US,en;q=0.9,he;q=0.8,ru;q=0.7",
+          "content-type": "application/json;charset=UTF-8",
+          "priority": "u=1, i",
+          "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
+          "sec-ch-ua-mobile": "?1",
+          "sec-ch-ua-platform": "\"Android\"",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "x-csrf-token": csrf_token,
+          "Referer": `https://qognify.bamboohr.com/employees/timesheet/?id=${employeeId}`,
+          "Referrer-Policy": "strict-origin-when-cross-origin",
+        },
+        body: body,
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error submitting timesheet entry: ${response.statusText}`);
+      }
+  
+      const data = await response.json();
+      console.log("Timesheet entry submitted successfully:", data);
+    } catch (error) {
+      console.error("Error submitting timesheet entry:", error);
+    }
+  }
+  
+
+
+async function processDayDataRequest2(requestDayData) {
     printLogContent('info', `processDayDataRequest: ${requestDayData.content}`);
     await sleep(1000);
     var dayData = JSON.parse(requestDayData.content);
@@ -107,11 +230,11 @@ async function processDayDataRequest(requestDayData) {
         
         transformHtmlElement(fabSelectElement, 'SM9.2.1 » BLD - Design, Code, QA, Documentation', '32-105');
 
-        let spanNotes = jssY61.querySelector('.AddEditEntry__noteIconWrapper');
-        let spanBaloon = spanNotes.firstChild;
-        if (spanBaloon) {
-            spanBaloon.setAttribute('balloon-position', 'top');
-          }
+        // let spanNotes = jssY61.querySelector('.AddEditEntry__noteIconWrapper');
+        // let spanBaloon = spanNotes.firstChild;
+        // if (spanBaloon) {
+        //     spanBaloon.setAttribute('balloon-position', 'top');
+        //   }
     }
     if (clockField1) {
         clockField1.focus();
@@ -147,10 +270,10 @@ function transformHtmlElement(element, textContent, optionValue) {
     
 
     // Get the select element
-    let select = element.querySelector('select');
-
+    let selectElement = element.querySelector('select');
+    selectElement.removeAttribute('data-has-default-value');
     // Change the value of the option
-    select.options[0].value = optionValue;
+    selectElement.options[0].value = optionValue;
 
     return element;
 }
@@ -261,7 +384,8 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         printLogContent('info', `Received "loadDayDataB2C" message with content: "${request.content}"`);
         if (request.command === "loadDayDataB2C") {
             printLogContent('info', `Received "loadDayDataB2C" message`);
-            processDayDataRequest(request);
+            const reqObject = JSON.parse(request.content);
+            processDayDataRequest(reqObject);
             printLogContent('info', `Processed "loadDayDataB2C" message`);
             sendResponse({ status: "success", message: "Message processed by content.js" });
         }
